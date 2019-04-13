fae3a730197f5521cc72c9bc9b9889e78370aa3d
Nick <walkmanaly@gmail.com>
2019-04-13 10:48:03 +0800

    day16 initialize project

---
+++ project1ofhackingwithswift/project1ofhackingwithswift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  project1ofhackingwithswift
+//
+//  Created by Nick on 2019/4/13.
+//  Copyright © 2019 Nick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
